7503786bb0755949b9ae6784219554d0e441c3eb
Kostya <kostya.papushnikov@yandex.ru>
2024-03-01 14:58:26 +0300

    update .gitignore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from typing import List
 from pydantic import BaseModel, Field
 
@@ -6,4 +7,5 @@ app = FastAPI(
     title='Delivery project'
 )
 
+templates = Jinja2Templates(directory="templates")
 
