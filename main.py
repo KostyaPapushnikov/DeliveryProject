6de19f79bfc10c0954e89458fa77b7e4bca95f02
Kostya <66370733+KostyaPapushnikov@users.noreply.github.com>
2024-05-09 20:35:06 +0300

    Куча всего

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI, Form, status, HTTPException, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from models import *
 from database import engine
 from sqlmodel import Session, select
 from typing import List, Annotated
 
-app = FastAPI()
+app = FastAPI(
+    description='Delivery Project'
+)
 
 templates = Jinja2Templates('templates')
 
 session = Session(bind=engine)
 
 
-@app.get('/', response_model=List[Food], status_code=status.HTTP_200_OK)
+
+@app.get('/', response_model=Food, tags=['Pages'])
 async def get_all_food(request: Request):
     statement = select(Food)
     result = session.exec(statement).all()
@@ -29,16 +32,7 @@ async def get_all_food(request: Request):
         }
     )
 
-@app.post('/food', response_model=Food, status_code=status.HTTP_201_CREATED)
-async def create_a_food(food: Annotated[Food, Depends()]):
-    new_food = Food(id=food.id, title=food.title, price=food.price)
-
-    session.add(new_food)
-    session.commit()
-    
-    return new_food
-
-@app.get('/food/{food_id}', response_model=Food)
+@app.get('/food/{food_id}', response_model=Food, tags=['Pages'])
 async def get_a_food(request: Request, food_id: int):
     statement = select(Food).where(Food.id == food_id)
     result = session.exec(statement).first()
@@ -55,6 +49,47 @@ async def get_a_food(request: Request, food_id: int):
             'price': result.price
         }
     )
+
+@app.get('/login', tags=['Pages'])
+async def get_login_page(request: Request):
+    return templates.TemplateResponse('login.html', {'request': request})
+
+@app.get('/registration', tags=['Pages'])
+async def get_registration_page(request: Request):
+    return templates.TemplateResponse('registration.html', {'request': request})
+
+@app.get('/profile/{cust_id}', response_model=Customer, tags=['Pages'])
+async def get_profile(request: Request, cust_id: int):
+    statement = select(Customer).where(Customer.id == cust_id)
+    result = session.exec(statement).first()
+
+    if result == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    return templates.TemplateResponse(
+        request = request,
+        name = 'profile.html',
+        context = {
+            'id': result.id,
+            'first_name': result.first_name,
+            'second_name': result.second_name,
+            'phone': result.phone,
+            'email': result.email,
+            'address': result.address
+        }
+    )
+
+
+
+
+@app.post('/food', response_model=Food, status_code=status.HTTP_201_CREATED)
+async def create_a_food(food: Annotated[Food, Depends()]):
+    new_food = Food(id=food.id, title=food.title, price=food.price)
+
+    session.add(new_food)
+    session.commit()
+    
+    return new_food
 
 @app.put('/food/{food_id}', response_model=Food)
 async def change_a_food(food_id: int, food: Annotated[Food, Depends()]):
@@ -82,54 +117,47 @@ async def delete_a_food(food_id: int):
     session.commit()
 
     return result
-    
-@app.get('/login')
-async def get_login_page(request: Request):
-    return templates.TemplateResponse('login.html', {'request': request})
 
-
-@app.get('/')
-async def get_index_page(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
-
-@app.post('/login', response_model=Customer, status_code=status.HTTP_201_CREATED)
-async def create_a_customer(cust: Annotated[Customer, Depends()]):
-    new_cust = Customer(id=cust.id, first_name=cust.first_name, second_name=cust.second_name,
-                        phone=cust.phone, email=cust.email, password=cust.password, address=cust.address)
+@app.post('/registration', status_code=status.HTTP_201_CREATED)
+async def create_a_customer(first_name: str = Form(...), 
+                            second_name: str = Form(...), 
+                            email: str = Form(...), 
+                            address: str = Form(...),
+                            phone: int = Form(...),
+                            password: str = Form(...)):
+    new_cust = Customer(first_name=first_name, second_name=second_name,
+                        phone=phone, email=email, password=password, address=address)
 
     session.add(new_cust)
     session.commit()
     
-    return new_cust
+    return RedirectResponse('/profile/'+str(new_cust.id), status_code=302)
 
-@app.get('/profile/{cust_id}', response_model=Customer)
-async def get_a_cust(request: Request, cust_id: int):
+
+@app.post('/login')
+async def get_cust(email_login: str = Form(...), password_login: str = Form(...)):
+    email_statement = select(Customer).where(Customer.email == email_login)
+    email_result = session.exec(email_statement).first()
+    
+    password_statement = select(Customer).where(Customer.password == password_login)
+    password_result = session.exec(password_statement).first()
+    
+    if email_result != None: 
+        if email_result.id == password_result.id:
+            return RedirectResponse('/profile/'+str(email_result.id), status_code=302)
+
+    
+
+@app.delete('/profile/{cust_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_a_cust(cust_id: int):
     statement = select(Customer).where(Customer.id == cust_id)
-    result = session.exec(statement).first()
+    result = session.exec(statement).one_or_none()
 
     if result == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail='Resourse Not Found')
     
-    return templates.TemplateResponse(
-        request = request,
-        name = 'cust_card.html',
-        context = {
-            'id': result.id,
-            'first_name': result.first_name,
-            'second_name': result.second_name,
-            'phone': result.phone,
-            'email': result.email,
-            'address': result.address
-        }
-    )
-    
-@app.post('/login', response_model=Customer)
-async def get_cust(request: Request, email: str = Form(...), password: str = Form(...)):
-    return templates.TemplateResponse(
-        request = request,
-        name = 'login.html',
-        context = {
-            'email': email,
-            'password': password
-        }
-    )
+    session.delete(result)
+    session.commit()
+
+    return result
